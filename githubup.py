b855a3ecfb2d882fc3e1dd36f85e2c4e4f3de149
froati <appmint456@gmail.com>
2026-04-16 15:52:26 +0900

    Update githubup.py

--- githubup.py
+++ githubup.py
@@ -4,9 +4,6 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-#
-from langchain_chroma import Chroma
-
 from langchain_community.vectorstores import FAISS
 
 from langchain_core.prompts import ChatPromptTemplate
